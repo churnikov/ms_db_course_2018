7a3728a3ae4e07b2b54eacf24e16551f6dbd2148
Nikita Churikov <nikita@chur.ru>
2018-11-29 16:01:56 +0300

    Add table creation script

---
+++ 2/3_create_table_employees.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees
+(
+employee_id  NUMBER(6),
+first_name  VARCHAR(20),
+last_name   VARCHAR(25),
+email       VARCHAR(25),
+sallary     NUMBER(8, 2)
+);
---
+++ 2/3_create_table_students.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students
+(
+student_id  NUMBER(6),
+first_name  VARCHAR(20),
+last_name   VARCHAR(25),
+email       VARCHAR(25)
+);
